4858051e1304a74c41075216f74f48eb4605c239
Luiz <luiz.mattos1@hotmail.com>
2015-11-18 19:38:57 -0200

    Criação do index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Manuseio de formulário com PHP</h1>
+        <h2>Referências:</h2>
+        <ul>
+            <li><a href="http://www.w3schools.com">W3Schools</a></li>
+            <li><a href="http://www.php.net">Manual do PHP</a></li>
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
