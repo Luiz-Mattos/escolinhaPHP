c6952650e0a11ccc492e5b7f8803b8ce5b1d624a
Luiz <luiz.mattos1@hotmail.com>
2015-11-18 20:35:37 -0200

    manuseio de formulários: http://www.w3schools.com/php/php_forms.asp

---
+++ enviaGET.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Exemplo de envio - Método GET</h1>
+        <form action="recebeget.php" method="get">
+            Name: <input type="text" name="name"><br>
+            E-mail: <input type="text" name="email"><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
